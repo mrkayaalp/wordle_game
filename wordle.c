a668122542930a7f5318e6c2a2b188ba4f6503fa
mrkayaalp <burakkayaalp52@gmail.com>
2023-02-25 13:19:17 +0300

    Kullanıcıdan kelime alımı

--- wordle.c
+++ wordle.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-
-
+void kelime_al(char tahmin[5]){
+    printf("5 harfli bir kelime tahmin edin: \t");
+    scanf("%s", tahmin);
+    }
 
 int main(){
+    int bulduk = 0;
+    int a = 0;
+    char tahmin[5];
+    
+    kelime_al(tahmin);
+    printf("%s\n", ptr);
+
     
 
 
