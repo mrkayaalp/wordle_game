34bccf95fee8b59c0c6cf2d20e347e7ac6e660df
mrkayaalp <burakkayaalp52@gmail.com>
2023-02-25 14:28:54 +0300

    oyunu loopa soktum ve tahmin sayısı ekledim

--- wordle.c
+++ wordle.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
 #include <string.h>
 
+#define tahmin_sayisi 6
 
 
 void kelime_al(char tahmin[5]){
     printf("5 harfli bir kelime tahmin edin: \t");
     scanf("%s", tahmin);
     }
+
 void oyun(char hedef[5], char tahmin[], char cikti[], char yanlis[], char olmayan[] ){
-    int bulduk = 0;
-    int a = 0;
-    int b = 0;
-    for(int i = 0; i<5; i++){
-        for(int j =0; j<5; j++){
-            if(tahmin[i] == hedef[j]){
-                bulduk = 1;
-                if(i == j)
-                    cikti[i] = tahmin[i];
-                else{
-                    yanlis[a] = tahmin[i];
-                    cikti[i] = '_';
-                    a++;
+    for(int c = 0; c < tahmin_sayisi; c++){
+        kelime_al(tahmin);
+        int bulduk = 0;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i<5; i++){
+            for(int j =0; j<5; j++){
+                if(tahmin[i] == hedef[j]){
+                    bulduk = 1;
+                    if(i == j)
+                        cikti[i] = tahmin[i];  
+                    else{
+                        yanlis[a] = tahmin[i];
+                        cikti[i] = '_';
+                        a++;
+                    }
                 }
             }
+            if(!bulduk){
+                cikti[i] = '_';
+                olmayan[b] = tahmin[i];
+                b++;
+            }
         }
-        if(!bulduk){
-            cikti[i] = '_';
-            olmayan[b] = tahmin[i];
-            b++;
-        }
+        printf("%s\n", cikti);
+        printf("Yeri yanlis olan harfler: %s\n", yanlis);
+        printf("Olmayan harfler: %s\n", olmayan);
     }
 }
 
+
 int main(){
     char tahmin[5];
-    char hedef[5];
+    char hedef[] = {"roket"};
     char cikti[] = {'_', '_', '_', '_', '_', '\0'};    
     char olmayan[5];
     char yanlis[5];
-    kelime_al(tahmin);
-    printf("%s\n", tahmin);
-    printf("%s\n", hedef);
+    
+    printf("%s", cikti);
+    
 
     
 
